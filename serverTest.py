8c8f90aa1d49a8d2db4dd11cbee936f38297753b
seaweed2018 <zhenbruin20@g.ucla.edu>
2023-10-25 02:47:31 -0700

    lab3

--- serverTest.py
+++ serverTest.py
@@ -5,17 +5,17 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
 
 # host = socket.gethostname()
-serv.bind(('0.0.0.0', 4444))
+serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
 	conn, addr = serv.accept()
-	from_client = ’’
+	from_client = ''
 	while True:
 		data = conn.recv(4096)
 		if not data: break
-		from_client += data.decode(’utf_8’)
+		from_client += data.decode('utf_8')
 		print(from_client)
 		conn.send("I am SERVER\n".encode())
 	conn.close()
-	print(’client disconnected’)
+	print('client disconnected')
  
